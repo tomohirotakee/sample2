87f41c03c9a78e0b0568864a36ae9e4b9ad2ee64
tomohiro <tomohiro.takee@icloud.com>
2020-07-10 07:51:09 +0000

    test

---
+++ views/admin/profile/edit.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>profile</title>
+    </head>
+    <body>
+        <h1>protein</h1>
+         <a herf = "https://96dc88cf7df14ae0b157ac1f9e4a0457.vfs.cloud9.us-east-2.amazonaws.com/admin/profile/edit">編集</a>
+    </body>
+</html>
