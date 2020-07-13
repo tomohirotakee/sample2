d3336f91add192e0651d21c0671542d0679d3e14
tomohiro <tomohiro.takee@icloud.com>
2020-07-13 09:54:36 +0000

    test

--- views/admin/news/create.blade.php
+++ views/admin/news/create.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.admin')
-@section('title','my　プロフィール')
+@section('title','ニュースの新規作成')
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>my プロフィール</h2>
+                <h2>ニュース新規作成</h2>
             </div>
         </div>
     </div>
